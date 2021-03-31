9c68361fb430ad8dda9e338f19e8972b36f9d6d2
Dhaval Sonani <dhavalsonani2921@yahoo.com>
2021-03-31 14:06:10 -0400

    Added .gitignore

--- models/admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AdminSchema = new mongoose.Schema({
-    	
-	    email_id:{type:String},
-
-})
-
-module.exports = mongoose.model('Admin', AdminSchema);
